126e8d085adf6e22e16b39c1a14d3bb9c6cc59ce
Besir Barca <besir.barca@one37id.com>
2023-10-07 15:35:49 +0300

    fs missing node standard library error fix

--- packages/agent-config/src/agentCreator.ts
+++ packages/agent-config/src/agentCreator.ts
@@ -1,6 +1,7 @@
 import { TAgent, IPluginMethodMap, IAgentOptions } from '@veramo/core'
 import { createObjects } from './objectCreator.js'
-import fs from 'fs'
+import * as FileSystem from 'expo-file-system';
+// import fs from 'fs'
 import yaml from 'yaml'
 
 /**
@@ -57,33 +58,60 @@ export async function createAgent<T extends IPluginMethodMap, C = Record<string,
  * Parses a yaml config file and returns a config object
  * @param filePath
  */
-export const getConfig = async (filePath: fs.PathLike): Promise<{ version?: number; [x: string]: any }> => {
-  let fileContent: string
+export const getConfig = async (filePath: string): Promise<{ version?: number; [x: string]: any }> => {
+  let fileContent: string;
 
-  // read file async
   try {
-    fileContent = await fs.promises.readFile(filePath, 'utf8')
+    fileContent = await FileSystem.readAsStringAsync(FileSystem.documentDirectory + filePath);
   } catch (e) {
-    console.log('Config file not found: ' + filePath)
-    console.log('Use "veramo config create" to create one')
-    process.exit(1)
+    console.log('Config file not found: ' + filePath);
+    console.log('Use "veramo config create" to create one');
+    throw e;
   }
 
-  let config
-
+  let config;
   try {
-    config = yaml.parse(fileContent, { prettyErrors: true })
+    config = yaml.parse(fileContent, { prettyErrors: true });
   } catch (e: any) {
-    console.error(`Unable to parse config file: ${e.message} ${e.linePos}`)
-    process.exit(1)
+    console.error(`Unable to parse config file: ${e.message}`);
+    throw e;
   }
 
   if (config?.version != 3) {
-    console.error('Unsupported configuration file version:', config.version)
-    process.exit(1)
+    const error = new Error('Unsupported configuration file version:' + config.version);
+    console.error(error);
+    throw error;
   }
-  return config
+
+  return config;
 }
+// export const getConfig = async (filePath: fs.PathLike): Promise<{ version?: number; [x: string]: any }> => {
+//   let fileContent: string
+
+//   // read file async
+//   try {
+//     fileContent = await fs.promises.readFile(filePath, 'utf8')
+//   } catch (e) {
+//     console.log('Config file not found: ' + filePath)
+//     console.log('Use "veramo config create" to create one')
+//     process.exit(1)
+//   }
+
+//   let config
+
+//   try {
+//     config = yaml.parse(fileContent, { prettyErrors: true })
+//   } catch (e: any) {
+//     console.error(`Unable to parse config file: ${e.message} ${e.linePos}`)
+//     process.exit(1)
+//   }
+
+//   if (config?.version != 3) {
+//     console.error('Unsupported configuration file version:', config.version)
+//     process.exit(1)
+//   }
+//   return config
+// }
 
 export async function getAgent<T extends IPluginMethodMap>(fileName: string): Promise<TAgent<T>> {
   try {
